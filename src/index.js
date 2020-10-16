92a1a54157ac56c00fc93800a2dcabf84fea4e47
ShaxxKh <sop.ugc2@gmail.com>
2020-10-16 16:47:05 +0500

    feat: implemented a function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let final = []
+if(matrix !== undefined){
+  for(var i = 0; i < matrix.length; i++){
+    if(i % 2 === 0){
+      for(var k = 0; k < matrix[i].length; k++){
+        final.push(matrix[i][k])
+      }
+    }
+    else {
+      var reversed = matrix[i].reverse()
+      for(var k = 0; k < matrix[i].length; k++){
+        final.push(reversed[k])
+      }
+    }
+  }
+  return final
+}
+else {
+  return []
+}
 }
